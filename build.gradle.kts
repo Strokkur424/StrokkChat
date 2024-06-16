fdbca859bd3e30dfd9443fed24ca8259c073b554
Strokkur24 <strokkur.24@gmail.com>
2024-06-16 23:09:25 +0200

    Setup gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,14 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     implementation("dev.jorel:commandapi-bukkit-shade:9.5.0")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("net.luckperms:api:5.4")
 }
 
 
